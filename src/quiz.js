604a2aa427af94107621c5a8a583bd483074d6bb
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-03-04 16:47:41 +0100

    day 1 done

--- src/quiz.js
+++ src/quiz.js
@@ -2,13 +2,41 @@ class Quiz {
   // YOUR CODE HERE:
   //
   // 1. constructor (questions, timeLimit, timeRemaining)
-  constructor(questions, timeLimit, timeRemaining) {}
-  // 2. getQuestion()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-  // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-  // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-  // 5. checkAnswer(answer)
-  // 6. hasEnded()
+  shuffleQuestions() {
+    let randomQ = [];
+    while (this.questions.length > 0) {
+      const qIndex = Math.floor(Math.random() * this.questions.length);
+      randomQ.push(this.questions.splice(qIndex, 1));
+    }
+    return randomQ;
+  }
+
+  checkAnswer(answer) {
+    if (answer) {
+      this.correctAnswers++;
+    }
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
