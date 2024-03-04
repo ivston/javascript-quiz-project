dbb4cf0279699de617eff56fac93e988ad7a1680
Claire Song <claireyuan.song@gmail.com>
2024-03-04 16:27:05 +0100

    Completed question day 1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  // 2. shuffleChoices()
+  shuffleChoices() {
+    let tempArr = [];
+    while (this.choices.length > 0) {
+      // randomizes a number between 1 and the number of choices
+      let index = Math.floor(Math.random() * this.choices.length);
+      // remove element from choices array and move it the temp array
+      tempArr.push(this.choices.splice(index - 1, 1));
 
-    // 2. shuffleChoices()
+      return tempArr;
+    }
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,14 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {}
+  // 2. getQuestion()
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 3. moveToNextQuestion()
 
-    // 4. shuffleQuestions()
+  // 4. shuffleQuestions()
 
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  // 6. hasEnded()
 }
